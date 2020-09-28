b37c00208bcde8fbadb4476c7ca28493ea753a84
David Wasson <davidcwasson@gmail.com>
2020-09-28 14:25:54 -0500

    Modified npm scripts, setup eslint, nodemon & ejs

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var express = require('express');
-var chalk = require('chalk'); // add color to console ouput
-var debug = require('debug')('app') // use instead of console.log
-var morgan = require('morgan'); // log web traffic to console
-var path = require('path'); // provides proper slashes in paths
+const express = require('express');
+const chalk = require('chalk'); // add color to console ouput
+const debug = require('debug')('app'); // use instead of console.log
+const morgan = require('morgan'); // log web traffic to console
+const path = require('path'); // provides proper slashes in paths
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny')); // use 'combined' if you want more information
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/index.html'));
-})
+app.get('/', (req, res) => {
+  res.render('index', { list: ['a', 'b'], title: 'Hello World' });
+});
 
-app.listen(3000, function(){
-  debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
 });
